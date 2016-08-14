a8013319a880d844c9fb4e1a376d9f62edc80943
zll <fist_zhang@126.com>
2016-08-14 16:59:56 +0800

    remove the temp file in the project, to optimize the version control

---
+++ TestCallAndroid/Assets/unEditableGameObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unEditableGameObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);  
+		//sphere.hideFlags = HideFlags.HideInHierarchy;  
+		//sphere.hideFlags = HideFlags.HideInInspector; 
+		//sphere.hideFlags = HideFlags.NotEditable; 
+		GameObject moveObj = GameObject.FindGameObjectWithTag("moveobj");
+		moveObj.hideFlags = HideFlags.NotEditable;  
+	}
+
+}
