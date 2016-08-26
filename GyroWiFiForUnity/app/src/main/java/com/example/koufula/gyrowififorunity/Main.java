820de10a02151437c4fc3fcf5cc7c0cadfcbcf06
yaolixiao003 <yaolixiao1219@gmail.com>
2016-08-26 09:40:36 +0800

    implment a gyro 2D mouse

--- GyroWiFiForUnity/app/src/main/java/com/example/koufula/gyrowififorunity/Main.java
+++ GyroWiFiForUnity/app/src/main/java/com/example/koufula/gyrowififorunity/Main.java
@@ -20,15 +20,88 @@ public class Main {
         new Thread(dataReceiveThread).start();
     }
 
-    public float getSensorInfoX() {
+    public float getGyroSensorInfoX() {
         if (dataReceiveThread != null) {
             SensorInfo si = dataReceiveThread.getSensorInfo();
             if (si != null) {
-                Log.d(DEBUG_TAG, "MainActivity: GyroX is:" + si.getmGyroX());
+                //Log.d(DEBUG_TAG, "MainActivity: GyroX is:" + si.getmGyroX());
                 return si.getmGyroX();
             }
             else {
-                Log.d(DEBUG_TAG, "MainActivity: SensorInfo is NULL");
+                //Log.d(DEBUG_TAG, "MainActivity: SensorInfo is NULL");
+                return INVALID_VALUE;
+            }
+        }
+        else {
+            Log.d(DEBUG_TAG, "MainActivity: dataReceiveThread is NULL");
+            return INVALID_VALUE;
+        }
+    }
+
+    public float getGyroSensorInfoY() {
+        if (dataReceiveThread != null) {
+            SensorInfo si = dataReceiveThread.getSensorInfo();
+            if (si != null) {
+                //Log.d(DEBUG_TAG, "MainActivity: GyroY is:" + si.getmGyroY());
+                return si.getmGyroY();
+            }
+            else {
+                //Log.d(DEBUG_TAG, "MainActivity: SensorInfo is NULL");
+                return INVALID_VALUE;
+            }
+        }
+        else {
+            Log.d(DEBUG_TAG, "MainActivity: dataReceiveThread is NULL");
+            return INVALID_VALUE;
+        }
+    }
+
+
+    public float getGyroSensorInfoZ() {
+        if (dataReceiveThread != null) {
+            SensorInfo si = dataReceiveThread.getSensorInfo();
+            if (si != null) {
+                //Log.d(DEBUG_TAG, "MainActivity: GyroZ is:" + si.getmGyroZ());
+                return si.getmGyroZ();
+            }
+            else {
+                //Log.d(DEBUG_TAG, "MainActivity: SensorInfo is NULL");
+                return INVALID_VALUE;
+            }
+        }
+        else {
+            Log.d(DEBUG_TAG, "MainActivity: dataReceiveThread is NULL");
+            return INVALID_VALUE;
+        }
+    }
+
+    public float getAccSensorInfoX() {
+        if (dataReceiveThread != null) {
+            SensorInfo si = dataReceiveThread.getSensorInfo();
+            if (si != null) {
+                //Log.d(DEBUG_TAG, "MainActivity: AccX is:" + si.getmAccX());
+                return si.getmAccX();
+            }
+            else {
+                //Log.d(DEBUG_TAG, "MainActivity: SensorInfo is NULL");
+                return INVALID_VALUE;
+            }
+        }
+        else {
+            Log.d(DEBUG_TAG, "MainActivity: dataReceiveThread is NULL");
+            return INVALID_VALUE;
+        }
+    }
+
+    public float getAccSensorInfoY() {
+        if (dataReceiveThread != null) {
+            SensorInfo si = dataReceiveThread.getSensorInfo();
+            if (si != null) {
+                //Log.d(DEBUG_TAG, "MainActivity: AccY is:" + si.getmAccY());
+                return si.getmAccY();
+            }
+            else {
+                //Log.d(DEBUG_TAG, "MainActivity: SensorInfo is NULL");
                 return INVALID_VALUE;
             }
         }
--- TestCallAndroid/Assets/gyrotest.cs
+++ TestCallAndroid/Assets/gyrotest.cs
@@ -8,26 +8,130 @@ public class gyrotest : MonoBehaviour {
 	public  InputField backup;
 	public  Text text;
 	private bool connect = false;
-	private float oldValue = 0;
-	private float newValue = 0;
-	private float minValue = 0;
-	private float maxValue = 0;
-	private const float maxY = 170;
-	private const float minY = 140;
-	private int flag = 1;
+	private float oldValueX = 0;
+	private float newValueX = 0;
+	private float oldValueY = 0;
+	private float newValueY = 0;
+	//private float minValue = 0;
+	//private float maxValue = 0;
+	private const float maxY = 162;
+	private const float minY = 145;
+	private const float maxX = 307;
+	private const float minX = 297;
+	private float flag = 1.5f;
+	private float mGyrox = 0;
+	private float mGyroy = 0;
+	//private float mGyroz = 0;
+	private float mAccx = 0;
+	private float mAccy = 0;
 
 	public GameObject cube;
 	private Vector3 cubecurrentposition;
 
     AndroidJavaObject mJO;
 
-    private float getSensorInfoX() {
-        //AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-        //AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject> ("currentActivity");
-
-        float gyrox = mJO.Call<float> ("getSensorInfoX");
+    private float getGyroSensorInfoX() {
+		float gyrox = mJO.Call<float> ("getGyroSensorInfoX");
         return gyrox;
     }
+
+	private float getGyroSensorInfoY() {
+		float gyroy = mJO.Call<float> ("getGyroSensorInfoY");
+		return gyroy;
+	}
+
+	private float getGyroSensorInfoZ() {
+		float gyroz = mJO.Call<float> ("getGyroSensorInfoZ");
+		return gyroz ;
+	}
+
+	private float getAccSensorInfoX() {
+		float accx = mJO.Call<float> ("getAccSensorInfoX");
+		return accx - 0.4f;
+	}
+
+	private float getAccSensorInfoY() {
+		//AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+		//AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject> ("currentActivity");
+
+		float accy = mJO.Call<float> ("getAccSensorInfoY");
+		return accy - 0.2f;
+	}
+
+	private void accMouse(){
+		mAccx = getAccSensorInfoX ();
+		mAccy = getAccSensorInfoY ();
+
+		oldValueX = newValueX;
+		newValueX = mAccx;
+		oldValueY = newValueY;
+		newValueY = mAccy;
+		float deltaX = newValueX - oldValueX;
+		float deltaY = newValueY - oldValueY;
+		cubecurrentposition = cube.transform.position;
+
+		if ( (mAccx > 0.2 || mAccx < -0.2) && (deltaX != 0)) {
+			print ("mAccx is"+mAccx);
+			cubecurrentposition.x = cube.transform.position.x + mAccx * flag;
+			if (cubecurrentposition.x > maxX) {
+				cubecurrentposition.x = maxX;
+			} else if (cubecurrentposition.x < minX) {
+				cubecurrentposition.x = minX;
+			}
+			print ("Cube position x is"+cubecurrentposition.x);
+		}
+
+		if ((mAccy > 0.2 || mAccy < -0.2) && (deltaY != 0)) {
+			//print ("mAccy is"+mAccy);
+			cubecurrentposition.y = cube.transform.position.y + mAccy * flag;
+			if (cubecurrentposition.y > maxY) {
+				cubecurrentposition.y = maxY;
+			} else if (cubecurrentposition.y < minY) {
+				cubecurrentposition.y = minY;
+			}
+			//print ("Cube position y is"+cubecurrentposition.y);
+		}
+
+		cube.transform.position = cubecurrentposition;
+	}
+
+	private void gyroMouse(){
+		
+		mGyrox = -getGyroSensorInfoZ ();
+		mGyroy = getGyroSensorInfoX ();
+		oldValueX = newValueX;
+		newValueX = mGyrox;
+		oldValueY = newValueY;
+		newValueY = mGyroy;
+		float deltaX = newValueX - oldValueX;
+		float deltaY = newValueY - oldValueY;
+
+		cubecurrentposition = cube.transform.position;
+	
+		if ( (mGyrox > 0.1 || mGyrox < -0.1) && (deltaX != 0)) {
+			print ("mGyrox is"+mGyrox);
+			cubecurrentposition.x = cube.transform.position.x + mGyrox * flag;
+			if (cubecurrentposition.x > maxX) {
+				cubecurrentposition.x = maxX;
+			} else if (cubecurrentposition.x < minX) {
+				cubecurrentposition.x = minX;
+			}
+			print ("Cube position x is"+cubecurrentposition.x);
+		}
+
+		if ((mGyroy > 0.1 || mGyroy < -0.1) && (deltaY != 0)) {
+			//print ("mGyroy is"+mGyroy);
+			cubecurrentposition.y = cube.transform.position.y + mGyroy * flag;
+			if (cubecurrentposition.y > maxY) {
+				cubecurrentposition.y = maxY;
+			} else if (cubecurrentposition.y < minY) {
+				cubecurrentposition.y = minY;
+			}
+			//print ("Cube position y is"+cubecurrentposition.y);
+		}
+
+		cube.transform.position = cubecurrentposition;
+	}
 
     void Start() {
         mJO = new AndroidJavaObject("com.example.koufula.gyrowififorunity.Main");
@@ -41,40 +145,13 @@ public class gyrotest : MonoBehaviour {
 			Application.Quit();
 		}
 		if (connect) {
-            float mGyrox = getSensorInfoX ();
-			backup.text = mGyrox.ToString ();
-			oldValue = newValue;
-			newValue = mGyrox;
-			float delta = newValue - oldValue;
-			if (delta > maxValue) {
-				maxValue = delta;
-			} else if (delta < minValue) {
-				minValue = delta;
-			}
-			//string maxs = maxValue.ToString ();
-			//string minx = minValue.ToString ();
-			//text.text = "max:" +  maxs + ", min:" + minx;
-
-
-			if (delta > 0.1 || delta < -0.1) {
-				text.text = delta.ToString();
-				cubecurrentposition = cube.transform.position;
-				cubecurrentposition.y = cube.transform.position.y + delta * flag;
-				if (cubecurrentposition.y > maxY) {
-					cubecurrentposition.y = maxY;
-					flag = flag * -1;
-				} else if (cubecurrentposition.y < minY) {
-					cubecurrentposition.y = minY;
-					flag = flag * -1;
-				}
-				cube.transform.position = cubecurrentposition;
-			}
+			gyroMouse ();
 		}
 	}
 
 	public void getGyroxInUnity(){
 		
-        float mGyrox = getSensorInfoX ();
+        float mGyrox = getGyroSensorInfoX ();
 		float fe1 = 100;
 		text.text = fe1.ToString();
 		//backup.text = fe1.ToString ();
